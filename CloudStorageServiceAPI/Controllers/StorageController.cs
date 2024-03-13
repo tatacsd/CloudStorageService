48ea611cf66ed42e10f95e86f0ed96c94e148d31
thayscasado <thays@0footprint>
2024-03-13 01:43:08 -0700

    Add project files.

---
+++ CloudStorageServiceAPI/Controllers/StorageController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorageServiceAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : ControllerBase
+{
+}
